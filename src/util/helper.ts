343ed57e4db6afcb0e45bd2db0567f829f3c6ba1
Asher <yyang.techie@gmail.com>
2021-06-11 23:04:07 -0400

    .

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -1,3 +1,5 @@
+
+
 /**
  * @description ### Returns Go / Lua like responses(data, err)
  * when used with await
@@ -19,9 +21,27 @@
  * @returns {Promise} [ data, undefined ]
  * @returns {Promise} [ undefined, Error ]
  */
-
 export const handle = (promise) => {
   return promise
     .then((data) => [data, undefined])
     .catch((error) => Promise.resolve([undefined, error]));
+};
+
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
+
+
+export const inspect = (obj:object):string => {
+  //stringify a circular structure in a JSON-like format
+  return JSON.stringify(obj, getCircularReplacer());
 };
